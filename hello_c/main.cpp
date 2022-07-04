84fa6b22748509e60f948a2ea12c9a2485e281c5
Mikael Lund <Mlund@mac.com>
2022-07-04 18:11:27 +0100

    Raster irq

--- hello_c/main.cpp
+++ hello_c/main.cpp
@@ -6,7 +6,9 @@ using byte = uint8_t;
 
 typedef void (*IRQ_TYPE)(void);
 IRQ_TYPE* const KERNAL_IRQ = (IRQ_TYPE*)0x0314;
+
 void irq() asm("IRQ"); // enable asm access
+void irq2() asm("IRQ2"); // enable asm access
 
 void loop_forever()
 {
@@ -15,7 +17,9 @@ void loop_forever()
     }
 }
 
-/** simplest possible interrupt(?) */
+/*
+ * simplest possible interrupt(?)
+ */
 void simple_interrupt(IRQ_TYPE irq)
 {
     asm volatile("sei");
@@ -23,9 +27,32 @@ void simple_interrupt(IRQ_TYPE irq)
     asm volatile("cli");
 }
 
+/*
+ * interrupt waiting for raster line
+ */
+void raster_wait_interrupt(IRQ_TYPE irq)
+{
+    asm volatile(
+        "sei\n"
+        "lda #$7f\n" // turn off the cia interrupts
+        "sta $dc0d\n"
+        "lda $d01a\n" // enable raster irq
+        "ora #$01\n"
+        "sta $d01a\n"
+        "lda $d011\n" // clear high bit of raster line
+        "and #$7f\n"
+        "sta $d011\n"
+        "lda #100\n" // line number to go off at
+        "sta $d012\n" // low byte of raster line
+    );
+    *KERNAL_IRQ = irq2;
+    asm volatile("cli");
+}
+
 int main()
 {
-    simple_interrupt(&irq);
+    //simple_interrupt(&irq);
+    raster_wait_interrupt(&irq);
     loop_forever();
 }
 
@@ -34,4 +61,19 @@ void irq()
     auto color = PEEK(0xd020);
     POKE(0xd020, color + 1);
     asm volatile("jmp $ea31");
+}
+
+int mode = 0;
+
+void irq2() {
+    auto color = PEEK(0xd020);
+    POKE(0xd020, color + 1);
+    asm volatile(
+            "lda $d019\n"
+            "sta $d019\n"
+            "lda #100\n"
+            "sta $d012\n"
+            "jmp $ea31"
+            );
+
 }
